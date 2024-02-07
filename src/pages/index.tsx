8e337e9b93b14f00fcbfdd4564d374b36c1c8a0e
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-02-07 21:45:20 +0700

    Update add new style
    Update add new style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 `}
+      className={`flex flex-col items-center overflow-scroll snap-y snap-mandatory justify-between `}
     >
-      awdawd
+      <div className=" snap-start bg-green-500 h-full min-h-[100svh] w-full">
+        awdaw
+      </div>
+      <div className=" snap-start bg-gray-500 h-full min-h-[100dvh] w-full">
+        awdaw
+      </div>
+      <div className=" snap-start bg-blue-500 h-full min-h-screen w-full">
+        wadwad
+      </div>
+      <div className="bg-red-500 fixed bottom-20 top-auto inset-x-0 mx-auto w-full max-w-[880px]">
+        awdaw
+      </div>
     </main>
   );
 }
