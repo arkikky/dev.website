1ebb370eaf6f9fde37bbe8188c89a988db34fe4d
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-02-07 23:32:10 +0700

    Update
    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,29 @@
 import Image from "next/image";
+import ReactPlayer from "react-player";
 
 export default function Home() {
   return (
     <main
       className={`flex flex-col items-center overflow-scroll snap-y snap-mandatory justify-between `}
     >
-      <div className=" snap-start bg-green-500 h-full min-h-[100svh] w-full">
+      <div className=" snap-start bg-green-500 h-full min-h-[100dvh] sm:min-h-screen max-h-[100dvh] w-full">
+        <ReactPlayer
+          playing
+          url="/ca2024HeroSection.mp4"
+          width="100%"
+          height="auto"
+        />
+      </div>
+      <div className=" snap-start bg-gray-500 h-full min-h-[100svh] sm:min-h-screen max-h-[100svh] w-full">
         awdaw
       </div>
-      <div className=" snap-start bg-gray-500 h-full min-h-[100dvh] w-full">
-        awdaw
-      </div>
-      <div className=" snap-start bg-blue-500 h-full min-h-screen w-full">
+      <div className=" snap-start bg-blue-500 h-full min-h-[dvh] sm:min-h-screen w-full">
         wadwad
       </div>
-      <div className="bg-red-500 fixed bottom-20 top-auto inset-x-0 mx-auto w-full max-w-[880px]">
+      <div className="bg-blue-500 fixed bottom-[11rem] top-auto inset-x-0 mx-auto h-[49px] w-full max-w-[80px]">
+        awdaw
+      </div>
+      <div className="bg-red-500 fixed bottom-14 top-auto inset-x-0 mx-auto h-[79px] w-full max-w-[880px]">
         awdaw
       </div>
     </main>
