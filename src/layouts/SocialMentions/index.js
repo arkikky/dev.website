5804a4a017eb7db3d6633e8270f68d8e324b841f
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-02-11 23:32:31 +0700

    build: improve website
    build: improve website

--- src/layouts/SocialMentions/index.js
+++ src/layouts/SocialMentions/index.js
@@ -19,56 +19,52 @@ const SocialMentions = ({ data }) => {
 
         {/* @content */}
         <Container className="overflow-hidden relative z-[5]">
-          <div className="flex flex-col overflow-hidden relative pt-[154px] sm:pt-[194px] pb-[254px] xl:pb-[172px]">
+          <div className="flex flex-col overflow-hidden relative pt-[154px] sm:pt-[194px] pb-[254px] xl:pb-[202px]">
             <div className="ca2024SocialMentionsContentTitle flex flex-col items-center justify-center opacity-1 transition duration-[1.2s] ease-out">
               <h2 className="text-black-900 font-staraExtraBold text-[32px] sm:text-[58px] lg:text-[80px] leading-[40px] sm:leading-[74px] lg:leading-[90px] text-center uppercase">
                 See what they're saying about Coinfest Asia
               </h2>
             </div>
 
-            <div className="ca2024SocialMentionsContent flex flex-col overflow-hidden relative transition duration-[2.2s] ease-out -mt-17 pointer-events-none lg:pointer-events-auto">
-              <div
-                ref={intGrabBackdrop}
-                id="ca2024SocialMentions"
-                className="ca2024SocialMentions relative mt-20 sm:mt-28 mb-28"
+            <div
+              ref={intGrabBackdrop}
+              id="ca2024SocialMentions"
+              className="ca2024SocialMentions bckdrpCvrShdow flex flex-col relative mt-20 sm:mt-28 mb-28 transition duration-[2.2s] ease-out pointer-events-none lg:pointer-events-auto"
+            >
+              <Splide
+                tag="section"
+                id="caSpldeSocialMentions"
+                aria-label="Social Mentions (Story)"
+                options={{
+                  direction: "ttb",
+                  type: "loop",
+                  drag: "free",
+                  perPage: 1,
+                  gap: "16px",
+                  arrows: false,
+                  pagination: false,
+                  keyboard: false,
+                  cloneStatus: false,
+                  height: "auto",
+                  autoScroll: {
+                    pauseOnFocus: false,
+                    rewind: false,
+                    speed: 2,
+                  },
+                }}
+                extensions={{ AutoScroll }}
+                className="caSpldeSocialMentions caSpldeMnsry overflow-hidden min-h-[1488px] max-h-[1488px] w-full"
               >
-                <div className={`bckdrpCvrShdow relative mt-11`}>
-                  <Splide
-                    tag="section"
-                    id="caSpldeSocialMentions"
-                    aria-label="Social Mentions (Story)"
-                    options={{
-                      direction: "ttb",
-                      type: "loop",
-                      drag: "free",
-                      perPage: 1,
-                      gap: "16px",
-                      arrows: false,
-                      pagination: false,
-                      keyboard: false,
-                      cloneStatus: false,
-                      height: "auto",
-                      autoScroll: {
-                        pauseOnFocus: false,
-                        rewind: false,
-                        speed: 2,
-                      },
-                    }}
-                    extensions={{ AutoScroll }}
-                    className="caSpldeSocialMentions caSpldeMnsry overflow-hidden min-h-[1488px] max-h-[1488px] w-full"
-                  >
-                    <SplideSlide className="outline-none focus:outline-none">
-                      <div className="caMnsry">
-                        {intSocialMentions?.map((getResult, index) => (
-                          <div key={index} className="break-inside-avoid">
-                            <CardSocialMentions {...getResult} />
-                          </div>
-                        ))}
+                <SplideSlide className="outline-none focus:outline-none">
+                  <div className="caMnsry">
+                    {intSocialMentions?.map((getResult, index) => (
+                      <div key={index} className="break-inside-avoid">
+                        <CardSocialMentions {...getResult} />
                       </div>
-                    </SplideSlide>
-                  </Splide>
-                </div>
-              </div>
+                    ))}
+                  </div>
+                </SplideSlide>
+              </Splide>
             </div>
           </div>
         </Container>
