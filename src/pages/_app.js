a4040862f04e581ea9f19942d3cf5b34eee74905
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-02-11 11:54:30 +0700

    test: user zoom
    test: user zoom

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }) {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0"
+            content="width=device-width, initial-scale=1.0, user-scalable=no"
           />
           <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
