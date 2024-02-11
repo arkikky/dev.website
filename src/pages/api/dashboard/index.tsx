66d17cf4fee81cbb8cae9dcf068a35626c55bffc
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-02-11 11:42:08 +0700

    build: clear repo
    build: clear repo

--- src/pages/api/dashboard/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  // Handle your API logic here
-
-  res.status(200).json({ message: "Dashboard data" });
-};
-
-export default handler;
