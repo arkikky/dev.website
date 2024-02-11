3f531325365d4dfe855db29ff66c58e1ca0679f1
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-02-11 12:18:43 +0700

    test: build
    test: build

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -20,10 +20,7 @@ export default function App({ Component, pageProps }) {
         <Head>
           <meta charSet="UTF-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
           <meta name="description" content={publicRuntimeConfig.siteDesc} />
@@ -63,7 +60,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
         <meta name="description" content={publicRuntimeConfig.siteDesc} />
