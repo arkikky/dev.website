c51ce4f6380f35585612cc5f78886de6b31f3306
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-02-11 13:18:25 +0700

    test
    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -94,10 +94,34 @@ export default function Home() {
           </div>
         </form>
       </div>
+      <form
+        id="icnSubsribeForm"
+        className="flex felx-col relative mt-6 lg:mt-0 w-full"
+        method="POST"
+      >
+        <div className="flex flex-col sm:flex-row relative w-full">
+          <input
+            id="icnEmailSubscrbie"
+            className="inline-flex border-none text-black-900 placeholder:text-black-900 font-figtree text-xl font-normal py-4 px-4 w-full"
+            type="email"
+            name="icnEmail"
+            placeholder="mymail@example.com"
+            required
+          />
+          <button
+            id="icnSubmitEmailSubscribe"
+            type="submit"
+            aria-label="ICN Submit Email Subscribe"
+            className="inline-flex flex-col items-center justify-center rounded-none sm:rounded-tr-3xl bg-black-900 text-white font-figtree text-base font-medium leading-inherit uppercase outline-none   focus-visible:outline-none py-4 px-[50px] w-full sm:w-max"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
 
       <div className="pt-32 px-4 sm:px-32 h-auto min-h-[auto] w-full">
         <form method="POST" className="flex flex-col w-full">
-          <div className="flex flex-col mb-4 last:mb-0">
+          <div className="flex flex-col mb-4 last:mb-0 w-full">
             <label
               htmlFor="inputFirstname"
               className="text-black-900 font-bevietnamPro text-base font-normal text-start mb-2"
@@ -112,7 +136,7 @@ export default function Home() {
               placeholder="Michael"
             />
           </div>
-          <div className="flex flex-col mb-4 last:mb-0">
+          <div className="flex flex-col mb-4 last:mb-0 w-full">
             <label
               htmlFor="inputFirstname"
               className="text-black-900 font-bevietnamPro text-base font-normal text-start mb-2"
