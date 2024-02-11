28d341b0d9d36126f976a0948750a57d6b0c8c87
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-02-11 20:22:16 +0700

    build: add indikator scroll down in header
    build: add indikator scroll down in header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -16,7 +16,8 @@ const Header = () => {
   return (
     <>
       <header className="snap-start snap-always relative h-svh xl:h-screen min-h-svh xl:min-h-screen z-[5]">
-        {/* <div className="flex flex-col items-center justify-center absolute top-auto bottom-28 sm:bottom-[142px] inset-0 mx-auto w-max z-[2]">
+        {/* @indikator */}
+        <div className="ca2024GrpBxShadow flex flex-col items-center justify-center absolute top-auto bottom-28 sm:bottom-[142px] inset-0 mx-auto w-max z-[2]">
           <div className="bg-white/50 rounded-[10px] flex flex-col items-center justify-center h-10 w-10">
             <div className="ca2024ScrolldownIcns">
               <div className="ca2024ScrolldownIcns-Indiktr motion-safe:animate-bounce"></div>
@@ -25,8 +26,9 @@ const Header = () => {
           <span className="text-white font-bevietnamPro text-base font-medium mt-4">
             Scroll to view more
           </span>
-        </div> */}
+        </div>
 
+        {/* @content */}
         <div className="ca2024BgVideo absolute inset-y-0 inset-x-0 pointer-events-none z-px">
           <video
             ref={isVideo}
