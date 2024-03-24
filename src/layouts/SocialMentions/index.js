6febaa7762ccd46d278e81ff31fcc7c99b87280a
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-03-24 14:06:40 +0700

    build: fix new assets backdrop, add new layout
    build: fix new assets backdrop, add new layout

--- src/layouts/SocialMentions/index.js
+++ src/layouts/SocialMentions/index.js
@@ -4,15 +4,15 @@ import Marquee from "react-fast-marquee";
 // @xomponents
 import Container from "@components/Container";
 // import CardSocialMentions from "@components/UI/Card/SocialMentions";
-import SocialMentionsCard from "@components/UI/Card/SocialMentions";
+// import SocialMentionsCard from "@components/UI/Card/SocialMentions";
 
 const SocialMentions = ({ data }) => {
   const [intSocialMentions, setSocialMentions] = useState(data);
 
   return (
     <>
-      <section className="ca2024MainPoints ca2024SocialMentionsPoints relative z-10 flex h-auto snap-start snap-always flex-col items-center justify-center overflow-hidden bg-white">
-        <div className="ca2024SocialMentionsPointTop pointer-events-none absolute inset-x-0 bottom-auto top-10 z-px h-14 w-full select-none bg-transparent transition-all duration-150 ease-linear"></div>
+      <section className="ca2024MainPoints ca2024SocialMentionsPoints relative z-10 flex h-auto snap-start snap-always flex-col items-center justify-center overflow-hidden bg-red-500">
+        {/* <div className="ca2024SocialMentionsPointTop pointer-events-none absolute inset-x-0 bottom-auto top-10 z-px h-14 w-full select-none bg-transparent transition-all duration-150 ease-linear"></div> */}
 
         {/* @content */}
         <Container className="relative z-[5] overflow-hidden">
@@ -23,25 +23,23 @@ const SocialMentions = ({ data }) => {
               </h2>
             </div>
 
-            <div
-              id="ca2024SocialMentions"
-              className="ca2024SocialMentions bckdrpCvrShdow pointer-events-none relative mb-28 mt-20 flex flex-col transition duration-[2.2s] ease-out sm:mt-28 lg:pointer-events-auto"
-            >
-              <Marquee
+            <div id="ca2024SocialMentions" className="bckdrpCvrShdow">
+              {/* <Marquee
                 autoFill={false}
                 pauseOnHover={false}
                 direction="down"
-                speed={30}
-                className="flex h-[600px] w-full flex-col"
-              >
-                <div className="ca2024Mnsry relative">
+                speed={30} 
+                className="flex w-full flex-col"
+              > */}
+              awdwdad
+              {/* <div className="ca2024Mnsry relative">
                   {intSocialMentions?.map((gtRslt, i) => (
                     <div className="break-inside-avoid" key={i}>
                       <SocialMentionsCard {...gtRslt} />
                     </div>
                   ))}
-                </div>
-              </Marquee>
+                </div> */}
+              {/* </Marquee> */}
             </div>
           </div>
         </Container>
---
+++ src/layouts/Tickets/index.js
@@ -0,0 +1,195 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+// @components
+import Container from "@components/Container";
+
+const Tickets = () => {
+  return (
+    <>
+      <section className="ca2024MainPoints ca2024StartTickets ca2024MainScreenAuto relative flex snap-start snap-always flex-col items-start justify-start overflow-hidden bg-secondary">
+        {/* @background (backdrop) */}
+        <div className="ca2024StartTicketsBackdrop opacity-1 absolute inset-x-0 inset-y-0 z-px">
+          <Image
+            className={`mx-auto h-full w-full object-cover object-center`}
+            src={"/assets/images/backdrop/ca2024BgLineRotate.jpg"}
+            alt={`Coinfest Asia 2024 (Background Backdrop Start Tickets)`}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
+            height={1262}
+            width={1440}
+            quality={87}
+          />
+        </div>
+
+        {/* @point-items (backdrop) */}
+        <div className="ca2024TicketsPoints opacity-1 pointer-events-none absolute -right-[225px] bottom-0 left-auto top-auto z-px select-none bg-transparent transition  duration-[0.8s] ease-out sm:-bottom-3 sm:-right-[283px] lg:-right-[197px] xl:-bottom-[253px] xl:-right-[120px]">
+          <Image
+            className="z-[2] mx-auto h-auto w-[671px] object-cover object-center sm:w-[971px] lg:w-[1080px]"
+            src={"/assets/images/backdrop/ca2024WidePointItems.png"}
+            alt={`Coinfest Asia 2024 (Wide Points Tickets)`}
+            height={1267}
+            width={1020}
+            quality="87"
+          />
+        </div>
+
+        <Container className="relative z-[5]">
+          <div className="relative flex flex-col overflow-hidden pb-[229px] pt-[135px]">
+            <div className="ca2024StartSpeakersContent flex flex-col text-start transition duration-[1.2s] ease-out">
+              <h2 className="w-ful max-w-full text-start font-staraExtraBold text-[44px] uppercase leading-[52px] text-white sm:max-w-[445px] sm:text-[54px] sm:leading-[74px] lg:max-w-[677px] lg:text-[80px] lg:leading-[90px]">
+                GET YOUR TICKETS NOW
+              </h2>
+            </div>
+            <div className="relative mt-8 grid-cols-4 gap-x-6 gap-y-6 supports-grid:grid sm:grid-cols-12 lg:grid-cols-12">
+              <div className="col-span-full lg:col-span-4">
+                <div className="ca2024product">
+                  <div className="ca2024ProductGroup ca2024ProductGroupHeading">
+                    <div className="z-[5] mr-3 flex flex-col">
+                      <Image
+                        className="h-[50px] w-full max-w-[16px] sm:h-[58px]"
+                        src={"/assets/images/backdrop/ca2024TicketsProduct.png"}
+                        alt="Coinfest Asia 2024 (Festival — Super Early Bird)"
+                        height={114}
+                        width={42}
+                        quality="87"
+                      />
+                    </div>
+                    <div className="relative z-[5] flex flex-col">
+                      <h3 className="z-[2] font-bevietnamPro text-base font-semibold sm:text-xl">
+                        Festival — Super Early Bird
+                      </h3>
+                      <div className="flex flex-row">
+                        <span className="sale-price z-[2] font-bevietnamPro text-base font-semibold sm:text-xl">
+                          $75
+                        </span>
+                        <span className="regular-price z-[2] ml-2 font-bevietnamPro text-base font-normal text-[#8A8A8A] line-through sm:text-xl">
+                          $150
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="ca2024ProductGroup ca2024ProductGroupContentFrmat">
+                    <ul>
+                      <li>2-day pass to Coinfest Asia main venue</li>
+                      <li>
+                        {`Access to main stage’s panel discussions &amp;
+                        conferences`}
+                      </li>
+                      <li>Direct networking with Speakers at Breakout Area</li>
+                      <li>Access to workshops, meetups, and side sessions</li>
+                      <li>{`Light food &amp; beverage for 2 days`}</li>
+                      <li>Access to selected Coinfest Week events</li>
+                      <li>Access to Official Networking Party</li>
+                    </ul>
+                  </div>
+
+                  <div className="ca2024ProductGroup ca2024productButton">
+                    <a
+                      href="https://ticket.coinfest.asia/?add-to-cart=9590949901"
+                      data-quantity="1"
+                      className="button product_type_simple add_to_cart_button ajax_add_to_cart"
+                      data-product_id="9590949901"
+                      data-product_sku=""
+                      aria-label="Add to cart: “Festival — Super Early Bird”"
+                      aria-describedby="Festival — Super Early Bird (Tickets)"
+                      rel="nofollow"
+                    >
+                      Buy ticket
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-full lg:col-span-4">
+                <div className="ca2024Product ca2024ProductVIP">
+                  <div className="ca2024ProductGroup ca2024ProductGroupHeading">
+                    <div className="z-[5] mr-3 flex flex-col">
+                      <Image
+                        className="h-[50px] w-full max-w-[16px] sm:h-[58px]"
+                        src={
+                          "/assets/images/backdrop/ca2024TicketsProductVIP.png"
+                        }
+                        alt="Coinfest Asia 2024 (Festival — Super Early Bird)"
+                        height={114}
+                        width={42}
+                        quality="87"
+                      />
+                    </div>
+                    <div className="relative z-[5] flex flex-col">
+                      <h3 className="z-[2] font-bevietnamPro text-base font-semibold sm:text-xl">
+                        Bull Ticket
+                      </h3>
+                      <div className="flex flex-row">
+                        <span className="sale-price z-[2] font-bevietnamPro text-base font-semibold sm:text-xl">
+                          $650
+                        </span>
+                        <span className="regular-price z-[2] ml-2 font-bevietnamPro text-base font-normal text-[#8A8A8A] line-through sm:text-xl">
+                          $700
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="ca2024ProductGroup ca2024ProductGroupContentFrmat">
+                    <ul>
+                      <li className="hightlight">
+                        All access from Festival Ticket
+                      </li>
+                      <li>Fast lane registration</li>
+                      <li>Access to Bull Area</li>
+                      <li>Exclusive access to the Speakers</li>
+                      <li>Dedicated food and drinks</li>
+                      <li>Access to selected VIP Coinfest Week events</li>
+                      <li>Fast lane access to Official Networking Party</li>
+                    </ul>
+                  </div>
+
+                  <div className="ca2024ProductGroup ca2024productButton">
+                    <a
+                      href="https://ticket.coinfest.asia/?add-to-cart=3613"
+                      data-quantity="1"
+                      className="button product_type_simple add_to_cart_button ajax_add_to_cart"
+                      data-product_id="3613"
+                      data-product_sku=""
+                      aria-label="Add to cart: “Bull Ticket”"
+                      aria-describedby="Bull (Tickets)"
+                      rel="nofollow"
+                    >
+                      Buy ticket
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center pt-20">
+              <Link
+                className="relative inline-flex w-full max-w-max items-center justify-center rounded-[14px] bg-primary px-6 py-4 font-bevietnamPro text-sm font-medium text-black-900 outline-none focus-visible:outline-none sm:text-xl"
+                href={"https://ticket.coinfest.asia/"}
+              >
+                Secure your spots now
+                <svg
+                  className="ml-1 h-6 w-6 sm:h-8 sm:w-8"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M8.66602 16.0013H23.3327M23.3327 16.0013L17.2151 10.668M23.3327 16.0013L17.2151 21.3346"
+                    stroke="#101010"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </Link>
+            </div>
+          </div>
+        </Container>
+      </section>
+    </>
+  );
+};
+
+export default Tickets;
