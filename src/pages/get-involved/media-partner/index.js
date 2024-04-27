3b366517328bafb5c42763caedf6b5698c478464
Dicky Pratama A <ikky.andreansyah@gmail.com>
2024-04-27 21:27:54 +0700

    build: fix button color in get-involved form
    build: fix button color in get-involved form

--- src/pages/get-involved/media-partner/index.js
+++ src/pages/get-involved/media-partner/index.js
@@ -650,7 +650,7 @@ const MediaPartnerGetInvolved = ({ ipAddrs, formMediaPartner }) => {
           <div className="flex flex-col">
             <button
               type="submit"
-              className={`mt-6 flex w-full flex-col items-center justify-center rounded-[14px] bg-secondary py-4 font-bevietnamPro text-base font-normal text-white outline-none transition duration-[0.3] ease-in-out focus-visible:outline-none`}
+              className={`form-input mt-6 flex w-full flex-col items-center justify-center rounded-[14px] bg-secondary py-4 font-bevietnamPro text-base font-normal text-white outline-none transition duration-[0.3] ease-in-out focus-visible:outline-none`}
               aria-label="Submit (Media Partner)"
             >
               Submit
