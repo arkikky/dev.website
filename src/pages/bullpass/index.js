1ed0d6849b041979b6cb1617dc2a9be573cb5960
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-07-26 13:03:54 +0700

    feat: update bull-pass
    feat: update bull-pass

---
+++ src/pages/bullpass/index.js
@@ -0,0 +1,283 @@
+import React from "react";
+import getConfig from "next/config";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+// @get .config
+const { publicRuntimeConfig } = getConfig();
+
+// @components
+import Container from "@components/Container";
+import BullPassCard from "@components/UI/Card/BullPassCard";
+
+// @layouts
+import BentoGridLayouts from "@layouts/BentoLayouts";
+
+const BullPass = () => {
+  return (
+    <>
+      {/* @head */}
+      <Head>
+        <title>{`Bull Pass | ${publicRuntimeConfig.siteTitle}`}</title>
+        <meta
+          name="title"
+          content={`Bull Pass | ${publicRuntimeConfig.siteTitle}`}
+        />
+        <meta name="description" content={publicRuntimeConfig.siteDesc} />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={publicRuntimeConfig.siteUrl} />
+        <meta
+          property="og:title"
+          content={`Bull Pass | ${publicRuntimeConfig.siteTitle}`}
+        />
+        <meta
+          property="og:description"
+          content={publicRuntimeConfig.siteDesc}
+        />
+        <meta
+          property="og:image"
+          content={`${process.env.NEXT_PUBLIC_UPLOAD}/uploads/ca2024_Thumbnails_Share_Link_App_9964b5c353.png`}
+        />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={publicRuntimeConfig.siteUrl} />
+        <meta
+          property="twitter:title"
+          content={`Bull Pass | ${publicRuntimeConfig.siteTitle}`}
+        />
+        <meta
+          property="twitter:description"
+          content={publicRuntimeConfig.siteDesc}
+        />
+        <meta
+          property="twitter:image"
+          content={`${process.env.NEXT_PUBLIC_UPLOAD}/uploads/ca2024_Thumbnails_Share_Link_App_9964b5c353.png`}
+        />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+
+      {/* @main */}
+      <BentoGridLayouts>
+        <main className="relative">
+          <header className="ca2024BgLineBlack relative mx-2 mt-2 flex h-[796px] flex-col items-center justify-center overflow-hidden rounded-[20px] bg-[#2B2B2B] sm:mx-2.5 sm:mt-3.5 sm:rounded-[26px] lg:mx-5 lg:mt-5 lg:h-[770px]">
+            {/* @points(flower) */}
+            <div className="absolute -left-[18px] bottom-0 right-auto top-auto z-px h-[181px] w-auto sm:-left-10 sm:h-[329px] lg:left-0 lg:h-[406px] xl:h-[459px]">
+              <Image
+                className="mx-auto h-full w-auto object-cover object-center"
+                src="/assets/images/backdrop/points/ca2024Points-BullPass-3.png"
+                alt={`Coinfest Asia 2024 (Left Leaf)`}
+                height={870}
+                width={1028}
+                quality="87"
+              />
+            </div>
+            <div className="absolute -right-1.5 bottom-0 left-auto top-auto z-px h-[121px] w-auto sm:-right-[47px] sm:h-[219px] lg:right-0 lg:h-[283px] xl:h-[383px]">
+              <Image
+                className="mx-auto h-full w-auto object-cover object-center"
+                src="/assets/images/backdrop/points/ca2024Points-BullPass-4.png"
+                alt={`Coinfest Asia 2024 (Right Leaf)`}
+                height={682}
+                width={1017}
+                quality="87"
+              />
+            </div>
+
+            <div className="relative z-[5] mt-1.5 flex w-full max-w-full flex-col text-center sm:max-w-[571px] lg:max-w-[657px] xl:max-w-[1367px]">
+              <span className="mx-auto mb-4 inline-flex w-max flex-row items-center justify-center rounded-full border border-solid border-white bg-white/[0.07] px-3 py-2 font-bevietnamPro text-sm font-light text-white sm:mb-6">
+                Bull Pass
+              </span>
+              <h1 className="font-staraExtraBold text-[36px] uppercase leading-[40px] text-white sm:text-[48px] sm:leading-[60px]">
+                Unlock the Ultimate VIP Experience
+              </h1>
+              <p className="mt-1 px-0 font-bevietnamPro text-base font-light text-white/80 lg:text-xl">
+                Experience elite perks & premium services at Asia's largest Web3
+                festival with Bull ticket
+              </p>
+              <div className="mx-auto mt-6 inline-flex w-full flex-col items-center justify-between px-4 sm:w-max sm:flex-row sm:px-0">
+                <button
+                  className={`relative mb-2 mr-0 inline-flex w-full items-center justify-center rounded-[14px] bg-white px-4 py-4 font-bevietnamPro text-base font-normal text-black-900 outline-none last:mr-0 focus-visible:outline-none sm:mb-0 sm:mr-4 sm:w-max sm:px-6`}
+                >
+                  Get bull pass
+                </button>
+                <Link
+                  className={`relative inline-flex w-full items-center justify-center rounded-[14px] bg-transparent px-4 py-4 font-bevietnamPro text-base font-normal text-white outline-none last:mr-0 hover:underline focus-visible:outline-none sm:w-max sm:px-6`}
+                  title="Coinfest Asia 2024 (Header BullPass)"
+                  href={""}
+                >
+                  Learn more about bull pass
+                </Link>
+              </div>
+            </div>
+          </header>
+
+          <div className="relative mt-12 flex flex-col">
+            <Container>
+              <div className="flex flex-col">
+                <h2 className="font-staraExtraBold text-[32px] uppercase leading-[38px] text-black-900">
+                  Perks
+                </h2>
+              </div>
+              <div className="mt-6 flex flex-col gap-x-6 gap-y-4 sm:gap-y-6">
+                <div className="relative grid-cols-1 gap-x-4 gap-y-4 supports-grid:grid sm:grid-cols-10 sm:gap-x-6 sm:gap-y-6 lg:grid-rows-1">
+                  <div className="col-span-full sm:col-span-5 sm:row-span-1 lg:row-span-2">
+                    <BullPassCard
+                      images="/assets/images/bull-pass/gallery/ca2024BentoGrid_BullPass1.jpg"
+                      title="Dedicated registration lane"
+                      shortDesc="Skip the lines and start your experience without the wait!"
+                      height={1950}
+                      width={1621}
+                    />
+                  </div>
+                  <div className="col-span-full h-full sm:col-span-5 lg:h-[320px] xl:h-[450px]">
+                    <BullPassCard
+                      images="/assets/images/bull-pass/gallery/ca2024BentoGrid_BullPass2.jpg"
+                      title="Bull Cave"
+                      shortDesc="Exclusive access to private area with all speakers. Network and engage with industry leaders in a secluded & comfortable setting."
+                      height={1275}
+                      width={2053}
+                    />
+                  </div>
+                  <div className="col-span-full hidden h-[201px] sm:col-span-1 lg:flex lg:flex-col">
+                    <div className="h-full overflow-hidden rounded-2xl">
+                      <Image
+                        className="z-[5] mx-auto h-full w-full object-cover object-center"
+                        src={
+                          "/assets/images/bull-pass/ca2024BgBentoGrid-Pin.png"
+                        }
+                        alt={`Coinfest Asia 2024 (Bento Grid - Pin)`}
+                        height={402}
+                        width={241}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-span-full sm:col-span-5 sm:row-span-1 lg:col-span-4 lg:row-span-2">
+                    <BullPassCard
+                      images="/assets/images/bull-pass/gallery/ca2024BentoGrid_BullPass4.jpg"
+                      title="Limited Edition Cold Wallet"
+                      shortDesc="Protect your investments with a high-quality, limited edition cold wallet provided by Carta"
+                      height={1300}
+                      width={1081}
+                    />
+                  </div>
+                  <div className="col-span-full h-full sm:col-span-5 lg:col-span-6 lg:h-[320px] xl:h-[450px]">
+                    <BullPassCard
+                      images="/assets/images/bull-pass/gallery/ca2024BentoGrid_BullPass3.jpg"
+                      title="Seamless Transportation"
+                      shortDesc="Travel around the event with ease and convenience with dedicated buggies."
+                      height={850}
+                      width={1369}
+                    />
+                  </div>
+                </div>
+                <div className="relative grid-cols-1 gap-x-4 gap-y-4 supports-grid:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-6 lg:grid-cols-3">
+                  <div className="col-span-full sm:col-span-1">
+                    <BullPassCard
+                      images="/assets/images/bull-pass/gallery/ca2024BentoGrid_BullPass5.jpg"
+                      title="Private bull party"
+                      shortDesc="Celebrate in style at the Private Bull Party with fellow Bull ticket holders and VIPs."
+                      normal={true}
+                      height={748}
+                      width={801}
+                    />
+                  </div>
+                  <div className="col-span-full sm:col-span-1">
+                    <BullPassCard
+                      images="/assets/images/bull-pass/gallery/ca2024BentoGrid_BullPass6.jpg"
+                      title="Personalized Matchmaking"
+                      shortDesc="Engage with top exhibitors and high-level executives tailored to your interests and goals."
+                      normal={true}
+                      height={748}
+                      width={801}
+                    />
+                  </div>
+                  <div className="col-span-full sm:col-span-1">
+                    <BullPassCard
+                      images="/assets/images/bull-pass/gallery/ca2024BentoGrid_BullPass7.jpg"
+                      title="Premium Bar & Café"
+                      shortDesc="Enjoy crafted beverages and personalized service in an exclusive lounge with private mixologist & barista"
+                      normal={true}
+                      height={748}
+                      width={801}
+                    />
+                  </div>
+                </div>
+              </div>
+            </Container>
+          </div>
+
+          {/* @banner */}
+          <section className="relative flex flex-col pt-[84px]">
+            <Container>
+              <div className="relative mt-0 flex h-full w-full flex-col items-start justify-center rounded-2xl bg-[#2B2B2B] px-4 py-8 sm:h-[483px] sm:rounded-[35px] sm:px-11 lg:px-17 xl:h-[614px]">
+                <div className="absolute inset-x-0 inset-y-0 z-px overflow-hidden rounded-2xl sm:rounded-[35px]">
+                  <Image
+                    className="z-[5] mx-auto h-full w-full object-cover object-center"
+                    src={
+                      "/assets/images/backdrop/background/ca2024BgLineBullPassBanner.jpg"
+                    }
+                    alt={`Coinfest Asia 2024 (Background Footer Banner)`}
+                    height={900}
+                    width={1440}
+                    quality="100"
+                  />
+                </div>
+
+                {/* @statue-items (backdrop) */}
+                {/* <div className="opacity-1 pointer-events-none absolute bottom-0 left-auto right-0 top-auto z-10 hidden select-none bg-transparent transition duration-[1s] ease-out xl:flex">
+                  <Image
+                    className="z-10 mx-auto h-auto w-[471px] object-cover object-center sm:w-[771px] lg:w-[702px]"
+                    src={
+                      "/assets/images/backdrop/statue/ca2024StatueBanner.png"
+                    }
+                    alt={`Coinfest Asia 2024 (Statue Footer Banner)`}
+                    height={976}
+                    width={753}
+                    quality="87"
+                  />
+                </div> */}
+
+                <div className="relative z-10 flex w-full max-w-[697px] flex-col items-start justify-start">
+                  <h2 className="font-staraExtraBold text-[32px] uppercase leading-[35px] text-white sm:text-[58px] sm:leading-[74px] xl:text-[80px] xl:leading-[90px]">
+                    Secure your Ultimate VIP Experience
+                  </h2>
+                  <div className="mt-4 flex w-full flex-col sm:mt-8 sm:w-max">
+                    <Link
+                      className="ca2024BgOverflayBullPass relative inline-flex w-full max-w-max items-center justify-center rounded-xl bg-primary px-3 py-3.5 font-bevietnamPro text-sm font-medium text-white outline-none focus-visible:outline-none sm:rounded-[14px] sm:px-6 sm:py-6 sm:text-xl"
+                      href={"https://ticket.coinfest.asia/"}
+                    >
+                      Get bull pass
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </Container>
+          </section>
+        </main>
+      </BentoGridLayouts>
+    </>
+  );
+};
+
+export default BullPass;
+
+export const getStaticProps = async () => {
+  try {
+    return {
+      props: {},
+
+      revalidate: 3600,
+    };
+  } catch (err) {
+    return {
+      notFound: true,
+    };
+  }
+};
+
+BullPass.getLayout = function PageLayout(page) {
+  return <>{page}</>;
+};
