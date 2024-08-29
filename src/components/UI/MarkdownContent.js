7bc662c2a2a0943de7ce44d5044353f92960777d
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-08-29 10:39:09 +0700

    feat: clear this repo
    feat: clear this repo

--- src/components/UI/MarkdownContent.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactMarkdown from "react-markdown";
-
-const MarkdownContent = ({ content }) => {
-  console.log(content);
-  // return <ReactMarkdown>{markdownContent}</ReactMarkdown>;
-};
-
-export default MarkdownContent;
