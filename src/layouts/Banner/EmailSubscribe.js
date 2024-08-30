e9314408804a4c3a896b14dcac9f5fedbfcb46e5
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-08-30 15:04:26 +0700

    feat: change to hubspot intregation
    feat: change to hubspot intregation

--- src/layouts/Banner/EmailSubscribe.js
+++ src/layouts/Banner/EmailSubscribe.js
@@ -3,7 +3,7 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 // @Controller
-import { Email } from "@lib/controller/Email";
+import { SubmitForm } from "@lib/controller/HubSpot";
 
 const BannerEmailSubscribe = () => {
   const intScBanner = useRef(null);
@@ -69,9 +69,26 @@ const BannerEmailSubscribe = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const rsFetch = await Email(intDtEmail);
+    const isConfig = {
+      fields: [
+        {
+          name: "email",
+          value: intDtEmail,
+        },
+      ],
+      context: {
+        pageUri: "https://coinfest.asia/",
+        pageName: "Coinfest Asia General",
+      },
+    };
 
-    if (rsFetch === true) {
+    const isKey = "c2b84fc6-adb1-4025-aa54-da644e049804";
+
+    console.log(isConfig);
+
+    const rs = await SubmitForm(isConfig, isKey);
+
+    if (rs === true) {
       setDtEmail("");
       mdlBtnSuccess.current.click();
     }
@@ -128,8 +145,8 @@ const BannerEmailSubscribe = () => {
             <button
               ref={mdlBtnSuccess}
               id="mdlBtnSuccess"
-              // className="toolsApp bg-white flex flex-col items-center justify-center cursor-pointer rounded-[10px] outline-none absolute top-full bottom-0 left-auto right-0 h-8 w-8 opacity-0 invisible z-10"
-              className="toolsApp bg-white flex flex-col items-center justify-center cursor-pointer rounded-[10px] outline-none absolute top-full bottom-0 left-auto right-0 h-8 w-8 z-10"
+              className="toolsApp bg-white flex flex-col items-center justify-center cursor-pointer rounded-[10px] outline-none absolute top-full bottom-0 left-auto right-0 h-8 w-8 opacity-0 invisible z-10"
+              // className="toolsApp bg-white flex flex-col items-center justify-center cursor-pointer rounded-[10px] outline-none absolute top-full bottom-0 left-auto right-0 h-8 w-8 z-10"
               aria-labelledby="Modal Subscribe Email"
               data-hs-overlay="#mdlSbscbeEmail"
             ></button>
