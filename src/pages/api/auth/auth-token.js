810345f733cb67a15092bc26bc831ae9454cf703
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-10-27 20:40:33 +0700

    feat: updated debug mode in /api/ route
    feat: updated debug mode in /api/ route

--- src/pages/api/auth/auth-token.js
+++ src/pages/api/auth/auth-token.js
@@ -6,39 +6,52 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 export default async function POST(req, res) {
+  const headersApiKey = req.headers['x-api-key'];
+
+  // @notification(error)
+  const errLog = [
+    {
+      data: null,
+      error: {
+        status: 405,
+        name: 'ForbiddenError',
+        message: 'Forbidden',
+      },
+    },
+  ];
+
   if (req.method !== 'POST') {
     console.warn('[warning]: method not allowed');
-    return res.status(405).json({ message: '[warning]: method not allowed' });
-  }
+    return res.status(405).json(errLog);
+  } else if (headersApiKey !== API_KEY) {
+    return res.status(403).json(errLog);
+  } else {
+    try {
+      const products = res.req.body;
 
-  const headersApiKey = req.headers['x-api-key'];
-  if (headersApiKey !== API_KEY) {
-    return res.status(403).json({ message: '[forbidden]: invalid API Key' });
-  }
+      const token = jwt.sign(
+        { data: products, action: 'addItems-toCart' },
+        JWT_SECRET,
+        {
+          expiresIn: '1h',
+        }
+      );
 
-  try {
-    const products = res.req.body;
-
-    const token = jwt.sign(
-      { data: products, action: 'addItems-toCart' },
-      JWT_SECRET,
-      {
-        expiresIn: '1h',
+      if (token) {
+        return res
+          .status(200)
+          .json({ token, accessCheckout: true, status: true });
+      } else {
+        return res.status(401).json({
+          accessCheckout: false,
+          respon: false,
+        });
       }
-    );
-
-    if (token) {
+    } catch (error) {
+      console.warn('[error]: internal Server Error');
       return res
-        .status(200)
-        .json({ token, accessCheckout: true, status: true });
-    } else {
-      return res.status(401).json({
-        accessCheckout: false,
-        respon: false,
-      });
+        .status(500)
+        .json({ message: '[error]: internal Server Error' });
     }
-  } catch (error) {
-    console.warn('[error]: internal Server Error');
-    return res.status(500).json({ message: '[error]: internal Server Error' });
   }
 }
