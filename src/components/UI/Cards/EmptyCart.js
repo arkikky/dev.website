d2813bc6a266c982ff9727d252bd1124c47882fe
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-12-18 12:47:36 +0700

    feat: update new beta version
    feat: update new beta version

---
+++ src/components/UI/Cards/EmptyCart.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Link from 'next/link';
+
+const EmptyCart = ({}) => {
+  return (
+    <>
+      <div className="flex w-full flex-col items-center justify-center rounded-xl bg-blue-50 px-6 py-6 text-center text-primary">
+        <div className="mb-3 flex h-11 w-11 flex-col items-center justify-center overflow-hidden rounded-full bg-blue-400/20">
+          <svg
+            className="h-7 w-7 shrink-0 stroke-primary"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15 5V7V5ZM15 11V13V11ZM15 17V19V17ZM5 5C4.46957 5 3.96086 5.21071 3.58579 5.58579C3.21071 5.96086 3 6.46957 3 7V10C3.53043 10 4.03914 10.2107 4.41421 10.5858C4.78929 10.9609 5 11.4696 5 12C5 12.5304 4.78929 13.0391 4.41421 13.4142C4.03914 13.7893 3.53043 14 3 14V17C3 17.5304 3.21071 18.0391 3.58579 18.4142C3.96086 18.7893 4.46957 19 5 19H19C19.5304 19 20.0391 18.7893 20.4142 18.4142C20.7893 18.0391 21 17.5304 21 17V14C20.4696 14 19.9609 13.7893 19.5858 13.4142C19.2107 13.0391 19 12.5304 19 12C19 11.4696 19.2107 10.9609 19.5858 10.5858C19.9609 10.2107 20.4696 10 21 10V7C21 6.46957 20.7893 5.96086 20.4142 5.58579C20.0391 5.21071 19.5304 5 19 5H5Z"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>
+          </svg>
+        </div>
+        <h3 className="text-base font-medium">Your Cart is Empty!</h3>
+        <p className="mt-0.5 w-full max-w-[374px] text-balance text-sm font-light">
+          Secure your tickets now and don’t miss out!
+        </p>
+        <Link
+          href="/"
+          className="mt-7 flex flex-row items-center justify-center gap-x-2 rounded-md text-sm font-medium leading-initial text-primary underline"
+        >
+          Buy Tickets
+          <svg
+            className="size-4.5 shrink-0 rotate-180 transform"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="m12 19-7-7 7-7" />
+            <path d="M19 12H5" />
+          </svg>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default EmptyCart;
---
+++ src/pages/_app.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import { Toaster } from 'sonner';
+import getConfig from 'next/config';
+import Head from 'next/head';
+
+// @get .config
+const { publicRuntimeConfig } = getConfig();
+
+// @style-css
+import '@styles/globals.css';
+
+// @redux
+import { Provider } from 'react-redux';
+import store from '@reduxState/store';
+
+// @lib/controller & helper
+import { useMethod } from '@lib/hooks/Method';
+
+// @script
+import PrelineScript from '@components/Script/PrelineScript';
+
+// @layouts
+import Layouts from '@layouts/Layouts';
+
+const App = ({ Component, pageProps }) => {
+  const { toggleOverlayPopUp } = useMethod();
+
+  // @with-layouts
+  const getLayout =
+    Component.getLayout ||
+    ((page) => (
+      <>
+        <Layouts cartStore={pageProps?.products}>
+          <Component {...pageProps} />
+        </Layouts>
+      </>
+    ));
+
+  // @without-layouts
+  return (
+    <>
+      <Provider store={store}>
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+          <meta name="description" content={publicRuntimeConfig.siteDesc} />
+          <meta name="author" content={'Coinfest Asia'} />
+          <link rel="mask-icon" href="/favicon.ico" color="#1F1F1F" />
+          <meta name="msapplication-TileColor" content="#1F1F1F" />
+          <meta name="theme-color" content="#F8FAFC" />
+          <meta name="msapplication-navbutton-color" content="#F8FAFC" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="#F8FAFC"
+          />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
+
+        {getLayout(<Component {...pageProps} />)}
+        <Toaster
+          position="bottom-left"
+          richColors
+          gap="10"
+          dismissible={false}
+          pauseWhenPageIsHidden={true}
+        />
+
+        {/* @pop-up(backcover) */}
+        <div
+          id="ca2025BckdrpOverflay_PopUpGeneral"
+          className="ca2025BckdrpOverflay_PopUpGeneral nonActive fixed inset-x-0 inset-y-0 z-[80] block h-svh cursor-pointer bg-black-900/60 backdrop-blur-[2px] transition-[opacity,backdrop-filter] duration-[0.3s] ease-in-out"
+          onClick={(e) => {
+            e.preventDefault();
+            toggleOverlayPopUp(
+              '.ca2025BckdrpOverflay_PopUpGeneral',
+              e.target.getAttribute('data-target')
+            );
+          }}
+        ></div>
+        {/* @pop-up(backcover) */}
+        <div
+          id="ca2025BckdrpOverflay_PopUpMobile"
+          className="ca2025BckdrpOverflay_PopUpMobile nonActive fixed inset-x-0 inset-y-0 z-[80] block h-svh cursor-pointer bg-black-900/60 backdrop-blur-[2px] transition-[opacity,backdrop-filter] duration-[0.3s] ease-in-out"
+          onClick={(e) => {
+            e.preventDefault();
+            toggleOverlayPopUp(
+              '.ca2025BckdrpOverflay_PopUpMobile',
+              e.target.getAttribute('data-target')
+            );
+          }}
+        ></div>
+
+        {/* @script */}
+        <PrelineScript />
+      </Provider>
+    </>
+  );
+};
+
+export default App;
