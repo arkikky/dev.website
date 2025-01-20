d76f328ecc14c70465aa641eda33c77093c5d32d
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2025-01-20 15:31:18 +0700

    feat: added new tracking code
    feat: added new tracking code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,7 +16,7 @@ export default function Document() {
         {/* @content-security-policy */}
         <meta
           httpEquiv="Content-Security-Policy"
-          content={`default-src 'self';script-src 'self' https://connect.facebook.net https://www.googletagmanager.com https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://www.google-analytics.com 'unsafe-eval' 'nonce-${hashNonce}';style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;font-src 'self' https://fonts.gstatic.com;connect-src 'self' https://coinfest.asia https://api.coinfest.asia https://api.hsforms.com https://hub.coinvestasi.com https://connect.facebook.net https://www.facebook.com https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://www.googletagmanager.com https://www.google-analytics.com https://checkout-staging.xendit.co https://vercel.live;img-src 'self' https://coinfest.asia https://upload.wikimedia.org https://api.coinfest.asia https://hub.coinvestasi.com https://flagcdn.com https://connect.facebook.net https://www.facebook.com https://t.co https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://www.googletagmanager.com https://www.google-analytics.com https://checkout-staging.xendit.co https://vercel.live data:;frame-src 'none';form-action 'self';worker-src 'self';base-uri 'self';`}
+          content={`default-src 'self';script-src 'self' https://connect.facebook.net https://www.googletagmanager.com https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://www.google-analytics.com 'unsafe-eval' 'nonce-${hashNonce}';style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;font-src 'self' https://fonts.gstatic.com;connect-src 'self' https://coinfest.asia https://api.coinfest.asia https://api.hsforms.com https://hub.coinvestasi.com https://connect.facebook.net https://www.facebook.com https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://www.googletagmanager.com https://www.google-analytics.com https://checkout-staging.xendit.co https://vercel.live;img-src 'self' https://coinfest.asia https://upload.wikimedia.org https://api.coinfest.asia https://hub.coinvestasi.com https://flagcdn.com https://connect.facebook.net https://www.facebook.com https://t.co https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://www.googletagmanager.com https://www.google-analytics.com https://checkout-staging.xendit.co https://vercel.live data:;frame-src 'self' https://www.googletagmanager.com;form-action 'self';worker-src 'self';base-uri 'self';`}
         />
         {/* @google-fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -30,14 +30,12 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@100;200;300;400;500;600;700;800;900&display=swap"
         />
 
-        {process.env.NODE_ENV === 'production' && (
-          <>
-            {/* @meta-pixel */}
-            <Script
-              id={`meta-pixel`}
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
+        {/* @meta-pixel */}
+        <Script
+          id={`meta-pixel`}
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
               !function(f,b,e,v,n,t,s)
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -49,52 +47,75 @@ export default function Document() {
               fbq('init', '534460966120098');
               fbq('track', 'PageView');
             `,
-              }}
-              nonce={hashNonce}
-            />
-            <noscript>
-              <Image
-                className={`!hidden`}
-                src="https://www.facebook.com/tr?id=534460966120098&ev=PageView&noscript=1"
-                height="1"
-                width="1"
-              />
-            </noscript>
+          }}
+          nonce={hashNonce}
+        />
+        <noscript>
+          <Image
+            className={`!hidden`}
+            src="https://www.facebook.com/tr?id=534460966120098&ev=PageView&noscript=1"
+            height="1"
+            width="1"
+          />
+        </noscript>
 
-            {/* @twitter(conversion-tracking) */}
-            <script
-              id={`twitter-conversion-tracking`}
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
+        {/* @twitter(conversion-tracking) */}
+        <script
+          id={`twitter-conversion-tracking`}
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
               !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
               },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
               a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
               twq('config','o9kyj');
             `,
-              }}
-              nonce={hashNonce}
-            />
+          }}
+          nonce={hashNonce}
+        />
 
-            {/* @google-tag (gtag.js) */}
-            <Script
-              id={`google-analytics`}
-              strategy="afterInteractive"
-              src={gtagUrl}
-              nonce={hashNonce}
-            />
-            <Script
-              id={`gtag-init`}
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: gtagScript,
-              }}
-              nonce={hashNonce}
-            />
-          </>
-        )}
+        {/* @google-tag (gtag.js) */}
+        <Script
+          id={`google-analytics`}
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-HC7BZC18HB`}
+          nonce={hashNonce}
+        />
+        <Script
+          id={`gtag-init`}
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; function gtag() { window.dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'G-HC7BZC18HB')`,
+          }}
+          nonce={hashNonce}
+        />
+        {/* @google-tag-manager */}
+        <Script
+          id={`google-tag-manager`}
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TMLM3MB');
+            `,
+          }}
+          nonce={hashNonce}
+        />
       </Head>
       <body>
+        {/* @google-tag-manager(noscript) */}
+        <noscript>
+          <iframe
+            className={`!hidden !h-0 !w-0`}
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-TMLM3MB`}
+            height="0"
+            width="0"
+          ></iframe>
+        </noscript>
+
         <Main />
         <NextScript nonce={hashNonce} />
       </body>
