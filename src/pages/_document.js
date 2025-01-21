c72eaca9b14ba976f669b94f980fb479f23c8ace
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2025-01-21 09:48:39 +0700

    feat: added new url csp tracking code
    feat: added new url csp tracking code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -26,7 +26,90 @@ export default function Document() {
         {/* @content-security-policy */}
         <meta
           httpEquiv="Content-Security-Policy"
-          content={`default-src 'self';script-src 'self' https://connect.facebook.net https://www.googletagmanager.com https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://www.google-analytics.com https://googleads.g.doubleclick.net https://static.hotjar.com https://script.hotjar.com 'unsafe-eval' 'sha256-+VpH0Cw80OuFaR+X1lmYjLkKsip/F2dDslL0JAvwlFA=' 'sha256-+kTPHgSOsYwYaXhSf2xTyBaBK4uVBsD6sgRbu0UKIYU=' 'sha256-h8qdr28t37PRaTPEFJ1DmRLJKbfRzrtcJDuEJUp1FBw=' 'sha256-lH5mF7jetR73rDA8fhru5w2+2TLgjsr3PXRS16nwqpY=' 'sha256-xm8N0GZArBHm4p9Zsd98vsHWVLP7knjZklGdLDzn35M=' 'sha256-63SgcioJg3SzunjNOZishsGbCME4jf2tExf3FX51aAA=' 'sha256-2kGECM9NFHBAhlNMIbyAm0Lu6VdF7TvhbzT9WW86kUI=' 'sha256-ANMpmVf8PLjCGynCggIAiTArAnaTMInBshu95qRJPzg=' 'sha256-BawaB+1tlg+I0FG2NG2/30p/CM96DWlvld7LzrS5/6M=' 'sha256-BzokaaX+EWQs7wVvCr0hpX7jNoWu847XT8ay8gsQVHA=' 'sha256-ciempzB86lmZWo8K/uPbojn0/dfNxls6k0PSaNLRvow=' 'sha256-n/RMhdYdR3bw+fP3SraQjHyjwYYKf9PKqEjbgGfhn+0=' 'sha256-eUnp2iVRt/owvyEacOFIV+gGEV6vsK65zTx3zVwaP7o=' 'sha256-DtxIxVxxirVV7lmJdKrGO2JEAskZPNSx7uZcXvTalWg=' 'sha256-ATLVdlL7dm0+nH5N56U4mqJ2hTLoPfU0SOGf4CSQsRg=' 'sha256-ZPfOsj5SKj5ldrOcWOWm2a5wUFeaCTcpC6Kg+D0A2+w=' 'sha256-9OW5aXYYSnZX5Dvg+19bDiia2eOhw0LCwR4r+V3V5d0=' 'sha256-hMTlXvm+Ql/aRmzjpffxtisA0FskENp0fgY/0O/63Qk=' 'sha256-WnXrrRzgpTOaoj+/CEeJjeYDfYu6qkeJB44k68IPQbs=';style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;font-src 'self' https://fonts.gstatic.com;connect-src 'self' https://coinfest.asia https://api.coinfest.asia https://api.hsforms.com https://hub.coinvestasi.com https://www.google.com https://www.google.com/ccm/collect https://connect.facebook.net https://www.facebook.com https://www.facebook.com/tr/ https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://www.googletagmanager.com https://www.google-analytics.com https://googleads.g.doubleclick.net https://static.hotjar.com https://script.hotjar.com https://metrics.hotjar.io https://content.hotjar.io wss://ws.hotjar.com https://checkout-staging.xendit.co https://vercel.live;img-src 'self' https://coinfest.asia https://upload.wikimedia.org https://api.coinfest.asia https://hub.coinvestasi.com https://flagcdn.com https://www.google.com https://www.google.co.id https://connect.facebook.net https://www.facebook.com https://www.facebook.com/tr/ https://t.co https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://www.googletagmanager.com https://www.google-analytics.com https://checkout-staging.xendit.co https://vercel.live data:;frame-src 'self' https://td.doubleclick.net https://www.googletagmanager.com;form-action 'self';worker-src 'self';base-uri 'self';`}
+          content={`default-src 'self';
+script-src 'self' 
+  https://connect.facebook.net 
+  https://www.googletagmanager.com 
+  https://static.ads-twitter.com 
+  https://ads-twitter.com 
+  https://ads-api.twitter.com 
+  https://analytics.twitter.com 
+  https://www.google-analytics.com 
+  https://googleads.g.doubleclick.net 
+  https://static.hotjar.com 
+  https://script.hotjar.com 
+  'unsafe-eval'
+  'sha256-+VpH0Cw80OuFaR+X1lmYjLkKsip/F2dDslL0JAvwlFA='
+  'sha256-+kTPHgSOsYwYaXhSf2xTyBaBK4uVBsD6sgRbu0UKIYU='
+  'sha256-h8qdr28t37PRaTPEFJ1DmRLJKbfRzrtcJDuEJUp1FBw='
+  'sha256-lH5mF7jetR73rDA8fhru5w2+2TLgjsr3PXRS16nwqpY='
+  'sha256-xm8N0GZArBHm4p9Zsd98vsHWVLP7knjZklGdLDzn35M='
+  'sha256-63SgcioJg3SzunjNOZishsGbCME4jf2tExf3FX51aAA='
+  'sha256-2kGECM9NFHBAhlNMIbyAm0Lu6VdF7TvhbzT9WW86kUI='
+  'sha256-ANMpmVf8PLjCGynCggIAiTArAnaTMInBshu95qRJPzg='
+  'sha256-BawaB+1tlg+I0FG2NG2/30p/CM96DWlvld7LzrS5/6M='
+  'sha256-BzokaaX+EWQs7wVvCr0hpX7jNoWu847XT8ay8gsQVHA='
+  'sha256-ciempzB86lmZWo8K/uPbojn0/dfNxls6k0PSaNLRvow='
+  'sha256-n/RMhdYdR3bw+fP3SraQjHyjwYYKf9PKqEjbgGfhn+0='
+  'sha256-eUnp2iVRt/owvyEacOFIV+gGEV6vsK65zTx3zVwaP7o='
+  'sha256-DtxIxVxxirVV7lmJdKrGO2JEAskZPNSx7uZcXvTalWg='
+  'sha256-ATLVdlL7dm0+nH5N56U4mqJ2hTLoPfU0SOGf4CSQsRg='
+  'sha256-ZPfOsj5SKj5ldrOcWOWm2a5wUFeaCTcpC6Kg+D0A2+w='
+  'sha256-9OW5aXYYSnZX5Dvg+19bDiia2eOhw0LCwR4r+V3V5d0='
+  'sha256-hMTlXvm+Ql/aRmzjpffxtisA0FskENp0fgY/0O/63Qk='
+  'sha256-WnXrrRzgpTOaoj+/CEeJjeYDfYu6qkeJB44k68IPQbs=';
+style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+font-src 'self' https://fonts.gstatic.com;
+connect-src 'self'
+  https://coinfest.asia
+  https://api.coinfest.asia
+  https://api.hsforms.com
+  https://hub.coinvestasi.com
+  https://www.google.com
+  https://www.google.com/ccm
+  https://connect.facebook.net
+  https://www.facebook.com
+  https://www.facebook.com/tr
+  https://static.ads-twitter.com
+  https://ads-twitter.com
+  https://ads-api.twitter.com
+  https://analytics.twitter.com
+  https://www.googletagmanager.com
+  https://www.google-analytics.com
+  https://googleads.g.doubleclick.net
+  https://www.googleadservices.com/pagead
+  https://static.hotjar.com
+  https://script.hotjar.com
+  https://metrics.hotjar.io
+  https://content.hotjar.io
+  wss://ws.hotjar.com
+  https://checkout-staging.xendit.co
+  https://vercel.live;
+img-src 'self' 
+  https://coinfest.asia
+  https://upload.wikimedia.org
+  https://api.coinfest.asia
+  https://hub.coinvestasi.com
+  https://flagcdn.com
+  https://www.google.com
+  https://www.google.co.id
+  https://connect.facebook.net
+  https://www.facebook.com
+  https://www.facebook.com/tr
+  https://t.co
+  https://static.ads-twitter.com
+  https://ads-twitter.com
+  https://ads-api.twitter.com
+  https://analytics.twitter.com
+  https://www.googletagmanager.com
+  https://www.google-analytics.com
+  https://checkout-staging.xendit.co
+  https://vercel.live
+  data:;
+frame-src 'self' https://td.doubleclick.net https://www.googletagmanager.com;
+form-action 'self';
+worker-src 'self';
+base-uri 'self';`}
         />
         {/* @google-fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
