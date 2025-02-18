df350c415fc04f84c9f99261f8c787bff5aaf609
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2025-02-18 18:15:43 +0700

    feat: test

--- src/lib/hooks/tracking-store/TrackingStore.js
+++ src/lib/hooks/tracking-store/TrackingStore.js
@@ -19,53 +19,76 @@ export function useTrackingStore() {
     );
 
     if (typeof window !== 'undefined' && window.gtag) {
-      window.dataLayer.push({
-        // event: 'purchase_confirmed',
-        // datalayer_event_name: 'purchase',
-        // // 'value.transID': order?.documentId,
-        // // 'value.transValue': order?.orderTotal,
-        // ecommerce: {
-        //   transaction_id: order?.documentId,
-        //   value: Number(order?.orderTotal),
-        //   tax: 0.11,
-        //   shipping: 0,
-        //   currency: 'IDR',
-        //   coupon: '-',
-        //   items: [
-        //     {
-        //       item_id: order?.products[0]?.id,
-        //       item_name: order?.products[0]?.name,
-        //       affiliation: 'coinfest.asia',
-        //       price: Number(order?.products[0]?.priceSale ?? order?.products[0]?.price),
-        //       item_brand: 'Coinfest Asia 2025',
-        //       item_variant: 'Festival Ticket',
-        //       item_variant_id: order?.products[0]?.documentId,
-        //       item_category: 'Tickets',
-        //       currency: 'IDR',
-        //       index: 0,
-        //       quantity: 1,
-        //       google_bussiness_vertical: 'retail',
-        //     },
-        //   ],
-        // },
-        event: 'purchase',
-        transaction_id: 'test-1234a',
-        value: 1000,
+      // window.dataLayer.push({
+      //   // event: 'purchase_confirmed',
+      //   // datalayer_event_name: 'purchase',
+      //   // // 'value.transID': order?.documentId,
+      //   // // 'value.transValue': order?.orderTotal,
+      //   // ecommerce: {
+      //   //   transaction_id: order?.documentId,
+      //   //   value: Number(order?.orderTotal),
+      //   //   tax: 0.11,
+      //   //   shipping: 0,
+      //   //   currency: 'IDR',
+      //   //   coupon: '-',
+      //   //   items: [
+      //   //     {
+      //   //       item_id: order?.products[0]?.id,
+      //   //       item_name: order?.products[0]?.name,
+      //   //       affiliation: 'coinfest.asia',
+      //   //       price: Number(order?.products[0]?.priceSale ?? order?.products[0]?.price),
+      //   //       item_brand: 'Coinfest Asia 2025',
+      //   //       item_variant: 'Festival Ticket',
+      //   //       item_variant_id: order?.products[0]?.documentId,
+      //   //       item_category: 'Tickets',
+      //   //       currency: 'IDR',
+      //   //       index: 0,
+      //   //       quantity: 1,
+      //   //       google_bussiness_vertical: 'retail',
+      //   //     },
+      //   //   ],
+      //   // },
+      //   event: 'purchase',
+      //   transaction_id: order?.documentId,
+      //   value: Number(order?.orderTotal),
+      //   shipping: 0,
+      //   currency: 'IDR',
+      //   coupon: '-',
+      //   ecommerce: {
+      //     transaction_id: 'test-123wda4',
+      //     value: 1000,
+      //     currency: 'IDR',
+      //     items: [
+      //       {
+      //         item_id: order?.products[0]?.id,
+      //         item_name: order?.products[0]?.name,
+      //         price: Number(
+      //           order?.products[0]?.priceSale ?? order?.products[0]?.price
+      //         ),
+      //         quantity: 2,
+      //       },
+      //     ],
+      //   },
+      // });
+
+      window.gtag('event', 'purchase', {
+        transaction_id: order?.documentId,
+        value: Number(order?.orderTotal),
+        shipping: 0,
         currency: 'IDR',
-        ecommerce: {
-          transaction_id: 'test-123wda4',
-          value: 1000,
-          currency: 'IDR',
-          items: [
-            {
-              item_id: 'test-item',
-              item_name: 'Test Ticket',
-              price: 500,
-              quantity: 2,
-            },
-          ],
-        },
+        coupon: '-',
+        items: [
+          {
+            item_id: order?.products[0]?.id,
+            item_name: order?.products[0]?.name,
+            price: Number(
+              order?.products[0]?.priceSale ?? order?.products[0]?.price
+            ),
+            quantity: 1,
+          },
+        ],
       });
+
       console.log('DataLayer Event Pushed:', window.dataLayer);
       return;
     }
