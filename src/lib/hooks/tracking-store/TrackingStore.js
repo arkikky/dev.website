a5a4f391029a1e63b812b366255a3def572d784c
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2025-02-18 14:29:21 +0700

    Update TrackingStore.js

--- src/lib/hooks/tracking-store/TrackingStore.js
+++ src/lib/hooks/tracking-store/TrackingStore.js
@@ -16,56 +16,37 @@ export function useTrackingStore() {
     console.log(order);
 
     if (typeof window !== 'undefined' && window.gtag) {
-      // window.dataLayer.push({
-      //   event: 'purchase_confirmed',
-      //   debug_mode: true,
-      //   'value.transID': order?.documentId,
-      //   'value.transValue': order?.orderTotal,
-      //   ecommerce: {
-      //     transaction_id: order?.documentId,
-      //     value: order?.orderTotal,
-      //     tax: 0.11,
-      //     shipping: 0,
-      //     currency: 'IDR',
-      //     coupon: '-',
-      //     items: [
-      //       {
-      //         item_id: order?.products[0]?.id,
-      //         item_name: order?.products[0]?.name,
-      //         item_brand: 'Coinfest Asia 2025',
-      //         price: order?.products[0]?.priceSale ?? order?.products[0]?.price,
-      //         item_category: 'Tickets',
-      //         quantity: 1,
-      //         id: order?.products[0]?.id,
-      //       },
-      //     ],
-      //   },
-      // });
-      window.gtag('event', 'purchase', {
-        transaction_id: order?.documentId,
-        value: order?.orderTotal,
-        tax: 0.11,
-        shipping: 0,
-        currency: 'IDR',
-        coupon: '-',
-        items: [
-          {
-            item_id: order?.products[0]?.id,
-            item_name: order?.products[0]?.name,
-            affiliation: 'Coinfset Asia',
-            index: 0,
-            item_brand: 'Coinfest Asia 2025',
-            item_category: 'Tickets',
-            item_list_id: 'tickets',
-            item_list_name: 'Tickets',
-            item_variant: 'Festival Ticket',
-            price: order?.products[0]?.priceSale ?? order?.products[0]?.price,
-            quantity: 1,
-          },
-        ],
+      window.dataLayer.push({
+        event: 'purchase_confirmed',
+        datalayer_event_name: 'purchase',
+        debug_mode: true,
+        'value.transID': order?.documentId,
+        'value.transValue': order?.orderTotal,
+        ecommerce: {
+          transaction_id: order?.documentId,
+          value: order?.orderTotal,
+          tax: 0.11,
+          shipping: 0,
+          currency: 'IDR',
+          coupon: '-',
+          items: [
+            {
+              item_id: order?.products[0]?.id,
+              item_name: order?.products[0]?.name,
+              affiliation: 'Coinfset Asia',
+              index: 0,
+              item_brand: 'Coinfest Asia 2025',
+              item_category: 'Tickets',
+              item_list_id: 'tickets',
+              item_list_name: 'Tickets',
+              item_variant: 'Festival Ticket',
+              price: order?.products[0]?.priceSale ?? order?.products[0]?.price,
+            },
+          ],
+        },
       });
 
-      console.log('DataLayer Event Pushed:', window.dataLayer);
+      // console.log('DataLayer Event Pushed:', window.dataLayer);
       return;
     }
   }, []);
