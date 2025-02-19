79f72236a3ef1b866ee3ee02625b5e319d57cf4b
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2025-02-19 14:56:41 +0700

    feat: test

--- src/lib/hooks/tracking-store/TrackingStore.js
+++ src/lib/hooks/tracking-store/TrackingStore.js
@@ -27,40 +27,40 @@ export function useTrackingStore() {
 
   // @purchase
   const trackingPurchase = useCallback(({ transID, transValue }) => {
-    if (typeof window !== 'undefined' && window.dataLayer) {
-      window.dataLayer.push({
-        event: 'purchase_confirmed',
-        'value.transID': transID,
-        'value.transValue': transValue,
-      });
-    }
+    // if (typeof window !== 'undefined' && window.dataLayer) {
+    //   window.dataLayer.push({
+    //     event: 'purchase_confirmed',
+    //     'value.transID': transID,
+    //     'value.transValue': transValue,
+    //   });
+    // }
   }, []);
   const handlePurchase = useCallback((order) => {
-    if (typeof window !== 'undefined' && window.gtag) {
-      window.gtag('event', 'purchase', {
-        transaction_id: 'Test_90_' + order?.documentId,
-        value: Number(order?.orderTotal),
-        shipping: 0,
-        currency: 'IDR',
-        coupon: '-',
-        items: [
-          {
-            item_id: `T-${order?.products[0]?.id}`,
-            item_name: 'Test Festival',
-            affiliation: 'Coinfest Asia',
-            currency: 'IDR',
-            item_brand: 'Coinfest Asia',
-            item_category: 'Festival Ticket',
-            price: Number(
-              order?.products[0]?.priceSale ?? order?.products[0]?.price
-            ),
-            quantity: 2,
-          },
-        ],
-      });
-      // console.log('DataL/ayer Event Pushed:', window.dataLayer);
-      return;
-    }
+    // if (typeof window !== 'undefined' && window.gtag) {
+    //   window.gtag('event', 'purchase', {
+    //     transaction_id: 'Test_90_' + order?.documentId,
+    //     value: Number(order?.orderTotal),
+    //     shipping: 0,
+    //     currency: 'IDR',
+    //     coupon: '-',
+    //     items: [
+    //       {
+    //         item_id: `T-${order?.products[0]?.id}`,
+    //         item_name: 'Test Festival',
+    //         affiliation: 'Coinfest Asia',
+    //         currency: 'IDR',
+    //         item_brand: 'Coinfest Asia',
+    //         item_category: 'Festival Ticket',
+    //         price: Number(
+    //           order?.products[0]?.priceSale ?? order?.products[0]?.price
+    //         ),
+    //         quantity: 2,
+    //       },
+    //     ],
+    //   });
+    //   // console.log('DataL/ayer Event Pushed:', window.dataLayer);
+    //   return;
+    // }
   }, []);
 
   return {
