b9f64f1cc7f42b35223dac9875e13ce5a65f1dcc
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2025-03-18 09:25:24 +0700

    feat: fixed meta pixel

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -35,23 +35,6 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@100;200;300;400;500;600;700;800;900&display=swap"
         />
 
-        {/* @meta-pixel */}
-        <Script
-          id={`meta-pixel`}
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '534460966120098');fbq('track', 'PageView');`,
-          }}
-          nonce={hashMetaPixelScript}
-        />
-        <noscript>
-          <Image
-            className={`!hidden`}
-            src="https://www.facebook.com/tr?id=534460966120098&ev=PageView&noscript=1"
-            height="1"
-            width="1"
-          />
-        </noscript>
-
         {/* @twitter(conversion-tracking) */}
         <Script
           id={`twitter-conversion-tracking`}
@@ -86,6 +69,23 @@ export default function Document() {
           }}
           nonce={hashGTagManager}
         />
+        {/* @meta-pixel */}
+        <Script
+          id={`meta-pixel`}
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: metaPixelScript,
+          }}
+          nonce={hashMetaPixelScript}
+        />
+        <noscript>
+          <Image
+            className={`!hidden`}
+            src="https://www.facebook.com/tr?id=534460966120098&ev=PageView&noscript=1"
+            height="1"
+            width="1"
+          />
+        </noscript>
       </Head>
       <body>
         <Main />
